8f8946ed3c8f4aaae0008bbd4099538b7d90d33f
Hrushikesh Dolas <92310365+hexadivine@users.noreply.github.com>
2026-06-01 22:38:27 +1000

    Update install.sh

--- rdp/kali/install.sh
+++ rdp/kali/install.sh
@@ -14,3 +14,18 @@ sudo docker run -d --name remote_kali_by_hexadivine \
   -p 3389:3389 \
   -v ~/.kali-rdp:/home/kali \
   remote_kali_by_hexadivine
+
+IP=$(ip -4 route get 1.1.1.1 | awk '{print $7; exit}')
+
+cat <<EOF > kali.desktop
+
+[Desktop Entry]
+Name=Kali
+Comment=Run commands on Kali via SSH
+Exec=sshpasswd -p kali ssh -p 22001 -Y -C -c chacha20-poly1305@openssh.com kali@$IP rofi
+Icon=distributor-logo-kali
+Terminal=false
+Type=Application
+Categories=Utility;
+
+EOF
