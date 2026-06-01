2e8cd45ed490946dccae7eeeb5af37a3067a9dc4
Hrushikesh Dolas <92310365+hexadivine@users.noreply.github.com>
2026-06-01 23:02:36 +1000

    Update install.sh

--- rdp/kali/install.sh
+++ rdp/kali/install.sh
@@ -12,6 +12,7 @@ sudo docker run -d --name remote_kali_by_hexadivine \
   --cap-add NET_ADMIN \
   --device /dev/net/tun \
   -p 3389:3389 \
+  -p 22001:22001 \
   -v ~/.kali-rdp:/home/kali \
   remote_kali_by_hexadivine
 
@@ -22,10 +23,12 @@ cat <<EOF > kali.desktop
 [Desktop Entry]
 Name=Kali
 Comment=Run commands on Kali via SSH
-Exec=sshpasswd -p kali ssh -p 22001 -Y -C -c chacha20-poly1305@openssh.com kali@$IP rofi
+Exec=sshpass -p kali ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null  -p 22001 -Y -C -c chacha20-poly1305@openssh.com kali@10.10.10.100 'rofi -show drun'
 Icon=distributor-logo-kali
 Terminal=false
 Type=Application
 Categories=Utility;
 
 EOF
+
+cat kali.desktop
